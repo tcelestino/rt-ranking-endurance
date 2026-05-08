4ef08dcab1497285996670073042ee42737c392a
Tiago Celestino <tcelestino@gmail.com>
2026-05-08 18:28:04 -0300

    feat: ajustando endpoint para capturar o ano

--- api/src/server.ts
+++ api/src/server.ts
@@ -39,9 +39,9 @@ function readJsonFile(filePath: string, res: express.Response): void {
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        res.status(404).json({ error: 'Arquivo não encontrado' });
+        res.status(404).json({ error: 'Dado não encontrado' });
       } else {
-        res.status(500).json({ error: 'Erro ao ler arquivo' });
+        res.status(500).json({ error: 'Erro ao ler os dados' });
       }
       return;
     }
@@ -51,6 +51,18 @@ function readJsonFile(filePath: string, res: express.Response): void {
       res.status(500).json({ error: 'Erro ao fazer o parse do JSON' });
     }
   });
+}
+
+function getDataByYear(year?: string): string {
+  const fullYear = !year ? new Date().getFullYear() : year;
+  return path.resolve(DATA_DIR, fullYear.toString());
+}
+
+function resultData(gender: 'male' | 'female', req: Request, res: Response) {
+  const { year, month } = req.params;
+  const data = getDataByYear(year as string);
+
+  readJsonFile(path.join(data, `${gender}-${month}.json`), res);
 }
 
 app.use(
@@ -80,14 +92,12 @@ app.get('/api/runners', (_req, res) => {
   readJsonFile(path.join(DATA_DIR, 'runners.json'), res);
 });
 
-app.get('/api/data/:month/female', validateMonth, (req, res) => {
-  const { month } = req.params;
-  readJsonFile(path.join(DATA_DIR, `female-${month}.json`), res);
+app.get('/api/data/:year/:month/female', validateMonth, (req, res) => {
+  resultData('female', req, res);
 });
 
-app.get('/api/data/:month/male', validateMonth, (req, res) => {
-  const { month } = req.params;
-  readJsonFile(path.join(DATA_DIR, `male-${month}.json`), res);
+app.get('/api/data/:year/:month/male', validateMonth, (req, res) => {
+  resultData('male', req, res);
 });
 
 app.listen(PORT, () => {
